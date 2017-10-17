94d45e9ef48f615aec8f2dab68fecbbc5357f63b
Aluno <mariaisabel_bastazini@live.com>
2017-10-17 08:55:42 -0300

    Quase pronto

--- app/src/main/java/com/example/aluno/myapp/MainActivity.java
+++ app/src/main/java/com/example/aluno/myapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.aluno.myapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,5 +20,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnC = (Button) findViewById(R.id.btnCadastrar);
         btnS = (Button) findViewById(R.id.btnSobre);
+        textLogin = (EditText) findViewById(R.id.TextLogin);
+        textSenha = (EditText) findViewById(R.id.textSenha);
+
+
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(
+                        MainActivity.this,
+                        cadastro.class
+
+                ));
+            }
+        });
+
+        btnS.setOnClickListener((new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(
+                        MainActivity.this,
+                        sobre.class
+                ));
+            }
+        }
+        ));
+
+        }
     }
-}
+
